0c0070b1447107e44effcad045a692e754516158
Rachid Boudjelida <rachidboudjelida@gmail.com>
2023-02-19 17:35:46 +0100

    remove contract details from referral card

--- src/configs/icoConfigs.js
+++ src/configs/icoConfigs.js
@@ -1,4 +1,4 @@
-const icoConfigs = {
+const icoConfigsGoerli = {
   router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
   factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
   ico: "0x47A10234C402A02e0f4c4853402e9AC408B6e3da",
@@ -34,7 +34,7 @@ const icoConfigs = {
 const icoConfigsBSC = {
   router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
   factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
-  ico: "",
+  ico: "0x4112a610FFBd3284DcB3f651B0F762c210A5260e",
   wbnb: {
     address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     symbol: "WBNB",
@@ -55,6 +55,13 @@ const icoConfigsBSC = {
     name: "WBNB-USDT",
     decimals: 18,
   },
+  solanter: {
+    address: "0xEE52bb1E2f5d2B4D5b9713EE32F6AF229e233948",
+    symbol: "SOLT",
+    name: "Solanter",
+    decimals: 18,
+  },
 };
-
+const icoConfigs =
+  process.env.REACT_APP_CHAIN_ID === "56" ? icoConfigsBSC : icoConfigsGoerli;
 export default icoConfigs;
