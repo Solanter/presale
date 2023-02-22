70fa49b6caa58b51afb97d1a71f17807c30ccde5
Rachid Boudjelida <rachidboudjelida@gmail.com>
2023-02-23 00:46:22 +0100

    bsc

--- src/configs/icoConfigs.js
+++ src/configs/icoConfigs.js
@@ -34,7 +34,7 @@ const icoConfigsGoerli = {
 const icoConfigsBSC = {
   router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
   factory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
-  ico: "0x4112a610FFBd3284DcB3f651B0F762c210A5260e",
+  ico: "0xBF41E8E871E96dAC3819367861a6AC50F633EDC2",
   wbnb: {
     address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     symbol: "WBNB",
